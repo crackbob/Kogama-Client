f6ce84820ffee95c933df3b847bed853123c3162
crackbob <crackbob.999@gmail.com>
2025-04-10 18:13:04 -0400

    refactor

--- src/memoryUtils.js
+++ src/memoryUtils.js
@@ -2,3 +2,14 @@ function read32(addr){return Module.HEAP32[addr/4];}
 function set32(addr,val){return Module.HEAP32[addr/4] = val;}
 function read8(addr){return Module.HEAP8[addr];}
 function set8(addr,val){return Module.HEAP32[addr] = val;}
+
+function read16(addr){return Module.HEAP16[addr/2];}
+function set16(addr,val){return Module.HEAP16[addr/2] = val;}
+
+function createString(value){
+    let ptr = Module._malloc(0xC+value.length*2);
+    writeString(ptr,value);
+    return ptr;
+}
+
+function writeString(ptr,str){set32(ptr+0x8,str.length);for(let i = 0;i<str.length*2;i+=2){set16(ptr+0xC+i,str.charCodeAt(i/2))}}
--- src/utils.js
+++ src/utils.js
@@ -1,30 +1,59 @@
-let sessionData = {
-    "isSoftLaunch": false,
-    "language": "en_US",
-    "embedded": true,
-    "gameRewardData": null,
-    "gameRewardURL": "https://api-www.kgoma.com/v1/api/reward/game-play/",
-    "gameRewardDataURL": "https://api-www.kgoma.com/v1/api/reward/game-data/",
-    "gamePublishedURL": "https://api-www.kgoma.com/v1/api/reward/published/",
-    "playerProfileURL": "https://www.kogama.com/profile",
-    "eliteUpgradeURL": "https://www.kogama.com/subscription/subscribe/",
-    "purchaseGoldURL": "https://www.kogama.com/purchase/",
-    "loginURL": "https://www.kogama.com/login/?next=https%3A%2F%2Fwww.kogama.com%2Fgames%2Fembed%2F7460414%2F%3Furl_referrer%3Dcrazygames.com",
-    "signupURL": "https://www.kogama.com/register/",
-    "idleURL": "https://www.kogama.com/page/disconnected/?reason=idle",
-    "disconnectedURL": "https://www.kogama.com/page/disconnected/",
-    "region": "eu",
-    "referrer": "kogama",
-    "detailedStats": false,
-    "isIOSDevice": false
-}
-
 function SendMessage (type, data) {
     Module.SendMessage("BrowserComm", type, JSON.stringify(data));
 }
 
-function UNITY_requestVideoAd (info) {
-    let callbackId = JSON.parse(info).callbackId;
+function UNITY_sendPlayerParams (data) {
+    let urlParams = new URLSearchParams(document.location.search);
+    let gameID = urlParams.get("gameID") || 7460414;
+    let profileID = 0;
+
+    let sessionCallbackID = JSON.parse(data).callbackId;
+    fetch(`https://www.kogama.com/locator/session/?objectID=${gameID}&profileID=${profileID}&lang=en_US&type=play`).then(async (response) => {
+        
+        let data = await response.json();
+        let sessionTokenID = JSON.parse(data.sessionToken.split(".")[0]).id;
+
+        let sessionData = {
+            "isSoftLaunch": false,
+            "language": "en_US",
+            "embedded": true,
+            "gameRewardData": null,
+            "gameRewardURL": "https://api-www.kgoma.com/v1/api/reward/game-play/",
+            "gameRewardDataURL": "https://api-www.kgoma.com/v1/api/reward/game-data/",
+            "gamePublishedURL": "https://api-www.kgoma.com/v1/api/reward/published/",
+            "playerProfileURL": "https://www.kogama.com/profile",
+            "eliteUpgradeURL": "https://www.kogama.com/subscription/subscribe/",
+            "purchaseGoldURL": "https://www.kogama.com/purchase/",
+            "loginURL": "https://www.kogama.com/login/?next=https%3A%2F%2Fwww.kogama.com%2Fgames%2Fembed%2F7460414%2F%3Furl_referrer%3Dcrazygames.com",
+            "signupURL": "https://www.kogama.com/register/",
+            "idleURL": "https://www.kogama.com/page/disconnected/?reason=idle",
+            "disconnectedURL": "https://www.kogama.com/page/disconnected/",
+            "region": "eu",
+            "referrer": "kogama",
+            "detailedStats": false,
+            "isIOSDevice": false,
+            "profileID": profileID,
+            "planetID": gameID,
+            "sessionToken": data.sessionToken,
+            "token": data.token,
+            "newPlanetName": data.sessionID,
+            "planetName": data.sessionID,
+            "pingURL": `https://www.kogama.com/locator/session/${sessionTokenID}/ping/?token=${data.sessionToken}`,
+            "disconnectURL": `https://www.kogama.com/locator/session/${sessionTokenID}/leave/?token=${data.sessionToken}`,
+            "unityPacketURL": `https://www.kogama.com/locator/session/${sessionTokenID}/leave/?token=${data.sessionToken}&plugin=WEBGL&ssl=1&unityPacket=1`,
+            "disconnectURL": `https://www.kogama.com/locator/session/${sessionTokenID}/leave/?token=${data.sessionToken}&profileID=${profileID}&objectID=${gameID}&lang=en_US&type=1&referrer=kogama&plugin=WEBGL&ssl=1&unityPacket=1`,
+            "serverIP": `wss://${data.hostName}:${data.wssPort}`
+        }
+
+        SendMessage("ExternalCallback", {
+            callbackId: sessionCallbackID,
+            data: JSON.stringify(sessionData)
+        });
+    });
+}
+
+function UNITY_requestVideoAd (data) {
+    let callbackId = JSON.parse(data).callbackId;
     SendMessage("ExternalCallback", {
         callbackId: callbackId,
         data: JSON.stringify({
@@ -33,8 +62,8 @@ function UNITY_requestVideoAd (info) {
     });
 }
 
-function UNITY_requestRewardedVideoAd (info) {
-    let callbackId = JSON.parse(info).callbackId;
+function UNITY_requestRewardedVideoAd (data) {
+    let callbackId = JSON.parse(data).callbackId;
     SendMessage("ExternalCallback", {
         callbackId: callbackId,
         data: JSON.stringify({
@@ -53,16 +82,8 @@ function UNITY_showRewardedVideoAd (data) {
     })
 }
 
-function UNITY_readyForAd () {
-
-}
-
-function UNITY_gamePlayStatus () {
-    
-}
-
-function UNITY_requestDomain (info) {
-    let callbackId = JSON.parse(info).callbackId;
+function UNITY_requestDomain (data) {
+    let callbackId = JSON.parse(data).callbackId;
     SendMessage("ExternalCallback", {
         callbackId: callbackId,
         data: JSON.stringify({
@@ -71,16 +92,8 @@ function UNITY_requestDomain (info) {
     });
 }
 
-function UNITY_unityDebugLog (info) {
-    console.debug(info);
-}
-
-function UNITY_sendPlayerParams (info) {
-    console.log(info);
-}
-
-function UNITY_sendLoadStats (info) {
-    let callbackId = JSON.parse(info).callbackId;
+function UNITY_sendLoadStats (data) {
+    let callbackId = JSON.parse(data).callbackId;
     SendMessage("ExternalCallback", {
         callbackId: callbackId,
         data: JSON.stringify({
@@ -90,57 +103,34 @@ function UNITY_sendLoadStats (info) {
     });
 }
 
-function get_browser_version (info) {
+function get_browser_version () {
     Module.SendMessage("BrowserComm", "GiveBrowserInfo", "Chrome,135");
-}
-
-function UNITY_sendStatHatCount () {
-    console.log(arguments);
-}
-
-function UNITY_sendPlayerParams (data) {
-
-    let urlParams = new URLSearchParams(document.location.search);
-    let gameID = urlParams.get("gameID") || 7460414;
-    let profileID = 0;
-
-    let sessionCallbackID = JSON.parse(data).callbackId;
-    fetch(`https://www.kogama.com/locator/session/?objectID=${gameID}&profileID=${profileID}&lang=en_US&type=play`).then(async (response) => {
-        let data = await response.json();
-
-        sessionData.profileID = profileID;
-        sessionData.planetID = gameID;
-        sessionData.profileID = profileID;
-
-        sessionData.sessionToken = data.sessionToken;
-        sessionData.token = data.token;
-
-        sessionData.newPlanetName = data.sessionID;
-        sessionData.planetName = data.sessionID;
-
-        let sessionTokenID = JSON.parse(data.sessionToken.split(".")[0]).id;
-        sessionData.pingURL = `https://www.kogama.com/locator/session/${sessionTokenID}/ping/?token=${data.sessionToken}`;
-        sessionData.disconnectURL = `https://www.kogama.com/locator/session/${sessionTokenID}/leave/?token=${data.sessionToken}`;
-        sessionData.unityPacketURL = `https://www.kogama.com/locator/session/${sessionTokenID}/leave/?token=${data.sessionToken}&plugin=WEBGL&ssl=1&unityPacket=1`;
-        sessionData.disconnectURL = `https://www.kogama.com/locator/session/${sessionTokenID}/leave/?token=${data.sessionToken}&profileID=${profileID}&objectID=${gameID}&lang=en_US&type=1&referrer=kogama&plugin=WEBGL&ssl=1&unityPacket=1`;
-
-        sessionData.serverIP = `wss://${data.hostName}:${data.wssPort}`;
-        
-        SendMessage("ExternalCallback", {
-            callbackId: sessionCallbackID,
-            data: JSON.stringify(sessionData)
-        });
-    });
 }
 
 function UNITY_gotoDisconnectedPage () {
     console.log("disconnected");
 }
 
-function UNITY_sendStatHatValue (info) {
-    console.log(info);
+function UNITY_sendStatHatValue () {
+
 }
 
-function UNITY_resetAFKtimer (info) {
-    console.log(info);
+function UNITY_readyForAd () {
+
+}
+
+function UNITY_sendStatHatCount () {
+    
+}
+
+function UNITY_gamePlayStatus () {
+    
+}
+
+function UNITY_resetAFKtimer () {
+    console.log("reset AFK timer");
+}
+
+function UNITY_unityDebugLog (data) {
+    console.debug(data);
 }
